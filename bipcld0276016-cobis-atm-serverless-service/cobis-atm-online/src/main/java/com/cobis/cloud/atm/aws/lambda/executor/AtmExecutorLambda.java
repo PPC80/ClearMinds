cb3a5daff95e4a64e3d0f2ce3f1df866f0a15b1c
Adrian Chicaiza <pedro_ppc@hotmail.com>
2022-12-16 10:38:26 -0500

    test

--- bipcld0276016-cobis-atm-serverless-service/cobis-atm-online/src/main/java/com/cobis/cloud/atm/aws/lambda/executor/AtmExecutorLambda.java
+++
@@ -1,7 +0,0 @@
-package com.cobis.cloud.atm.aws.lambda.executor;
-
-import com.cobis.cloud.cts.handler.aws.InfrastructureLogicExecutor;
-
-public class AtmExecutorLambda extends InfrastructureLogicExecutor {
-
-}
